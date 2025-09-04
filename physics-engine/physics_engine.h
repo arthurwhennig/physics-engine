894a6e686eae0b2b0b794bc739066cec44df8cd2
Arthur Hennig <arthur.w.hennig@gmail.com>
2025-09-04 11:58:48 +0200

    Initial Commit

---
+++ physics-engine/physics_engine.h
@@ -0,0 +1,18 @@
+//
+//  physics_engine.h
+//  physics-engine
+//
+//  Created by Arthur Hennig on 04.09.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for physics_engine.
+FOUNDATION_EXPORT double physics_engineVersionNumber;
+
+//! Project version string for physics_engine.
+FOUNDATION_EXPORT const unsigned char physics_engineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <physics_engine/PublicHeader.h>
+
+
